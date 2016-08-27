722043a24ef20ca2e40b442461aaa5ba0e7f75f6
dave-read <davre@microsoft.com>
2016-08-27 15:40:17 -0700

    initial check in

---
+++ containerizing-apps/web-node/server.js
@@ -0,0 +1,39 @@
+var os = require('os');
+var request = require('request');
+var morgan = require('morgan');
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(require("morgan")("dev"));
+
+// #######################################
+// Default content page
+// #######################################
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// #######################################
+// Call the WebAPI for information
+// #######################################
+// build up the URL using environment info
+var apiPort = process.env.APIPORT || 5000
+var apiURL = process.env.APIURL || "localhost"
+var requestApi = "http://"+apiURL+":"+apiPort+"/api/hello";
+
+app.get('/api', function (req, res) {
+    request(requestApi, function (error, response, body) {
+        res.send('From api-dotnet: ' + body);
+    });    
+});
+
+// #######################################
+// Start the web server
+// #######################################
+// Set the web server listenting port, based on an environment var
+// default to the local 3000 port
+var port = process.env.HTTPPORT || 3000;
+app.listen(port, function () {
+    console.log("Listening on port " + port);
+});
