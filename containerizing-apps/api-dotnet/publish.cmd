4c6cb3d861e3a729c7ea7dc3e2094908a298c675
dave-read <davre@microsoft.com>
2016-08-28 18:22:17 -0700

    update dockerfiles

--- containerizing-apps/api-dotnet/publish.cmd
+++ containerizing-apps/api-dotnet/publish.cmd
@@ -2,6 +2,6 @@
 set framework=netcoreapp1.0
 set runtimeID=debian.8-x64
 set Environment=Debug
-set pubFolder="bin\%Environment%\%framework%\publish"
+set pubFolder="bin/%Environment%/%framework%/publish"
 
 dotnet publish -f %framework% -r %runtimeID% -c %Environment% -o %pubFolder%
