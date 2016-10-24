aae3bdd1fd78a63cad887e6a475e92775270e110
dave-read <davre@microsoft.com>
2016-10-23 22:57:16 -0700

    updates for docker datacenter
    1, make web client context path aware
    2. add properties for proxy context path routing

--- containerizing-apps/web-node/public/app.js
+++ containerizing-apps/web-node/public/app.js
@@ -1,9 +1,20 @@
-var app = angular.module('myApp', ['ngRoute']);
-    app.controller('MainController', function($scope, $http) {
+var app = angular.module('myApp', ['ngRoute'],function($locationProvider){
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
+    app.controller('MainController', function($scope, $http, $location) {
 
     $scope.messages = [];
     $scope.sayHelloToServer = function() {
-        $http.get("/api").then(function(response) {
+        var path = $location.path().toLowerCase();
+        var requestPath = "/api";
+        // check of there's a context path
+        if (path !== "/" && path !== "/index.html") {
+          requestPath = "/"+path.split("/")[1] + requestPath
+        }
+        $http.get(requestPath).then(function(response) {
             $scope.messages.push(response.data);
         });
     };
